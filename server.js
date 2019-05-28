a54a4e408c37706136c3a06b6cbd5b09428cdfd8
David <daviid.saal@gmail.com>
2019-05-28 16:35:27 +0300

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 var result, accept_language, time, flag, time_zone;
-var country, fullCountry, answer, ans, mobile, navigator;
+var country, fullCountry, answer, ans;
 var test1,test2,test3;
 var check1,check2,check3;
 var percent1,percent2,percent3;
@@ -25,10 +25,9 @@ app.get('/', function(req, res) {
     console.log("\n------------------------------------------------------");
     //ipClient = '217.182.175.75'; //Proxy
     //ipClient = '104.248.140.7'; //VPN
-    ipClient = '109.64.87.92'; //Real IP
-    //ipClient = req.header('x-forwarded-for');
+    //ipClient = '109.64.87.92'; //Real IP
+    ipClient = req.header('x-forwarded-for');
     accept_language = req.header('accept-language');
-    navigator = req.header('user-agent');
     country = geoip.lookup(ipClient)['country'];
     fullCountry = CountryLanguage.getCountry(country).name;
     console.log("Client Connected..");
@@ -86,14 +85,6 @@ function HostChecker(res)
       check2 = 'Checking Error';
     }
     else{
-      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        console.log('asdasdasdasdasdasdasdasd');
-        //mobile = true;
-      }
-      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator)) {
-        console.log('asdasdasdasdasdasdasdasd');
-        //mobile = true;
-      }
       ans = 0;
       ipNumbers = ipClient.split('.');
       replace = ipNumbers[0]+'-'+ipNumbers[1]+'-'+ipNumbers[2]+'-'+ipNumbers[3];
